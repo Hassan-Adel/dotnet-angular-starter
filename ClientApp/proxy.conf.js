824d56540a120c4f5498f8dc38d9a39c4ca7edd2
Hassan-Adel <hasanadel555@hotmail.com>
2023-03-26 18:17:06 +0200

    Add proxy rules

--- ClientApp/proxy.conf.js
+++ ClientApp/proxy.conf.js
@@ -6,11 +6,18 @@ const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_H
 const PROXY_CONFIG = [
   {
     context: [
-      "/api/*",
       "/swagger",
       "/weatherforecast",
       "/WeatherForecast",
    ],
+    target: target,
+    secure: false,
+    headers: {
+      Connection: 'Keep-Alive'
+    }
+  },
+  {
+    context: ['/api/**'],
     target: target,
     secure: false,
     headers: {
